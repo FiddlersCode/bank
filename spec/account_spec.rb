ee4757f2c4fd99536e61e3a45020074e4746d2e0
Paula Muldoon <pmuldoon@umich.edu>
2017-06-12 11:30:11 +0100

    deposit method added, tests pass

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,9 @@ describe 'account' do
   it 'should start with a balance of zero' do
     expect(account.balance).to equal(0)
   end
+
+  it 'should accept a deposit' do
+    account.deposit(1000)
+    expect(account.balance).to equal(1000)
+  end
 end
