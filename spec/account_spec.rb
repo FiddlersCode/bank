26c9d118abd18ee75d7c5050b3850e1a41419e8b
Paula Muldoon <pmuldoon@umich.edu>
2017-06-12 12:01:05 +0100

    added hook, contexts to tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,28 +3,40 @@ require 'account'
 describe 'account' do
   let(:account) { Account.new }
   let(:date) { Time.now.strftime "%d/%m/%Y" }
-  it 'should start with a balance of zero' do
-    expect(account.balance).to equal(0)
+
+  context 'initialize' do
+    it 'should start with a balance of zero' do
+      expect(account.balance).to equal(0)
+    end
   end
 
-  it 'should accept a deposit' do
-    account.deposit(1000)
-    expect(account.balance).to equal(1000)
+  context 'account actions requiring deposit' do
+    before(:each) do
+      account.deposit(1000)
+    end
+
+    it 'should accept a deposit' do
+      expect(account.balance).to equal(1000)
+    end
+
+    it 'should show the date of the deposit' do
+      'expect(account.withdrawal_date).to equal(date)'
+    end
+
+    it 'should issue a withdrawal' do
+      account.withdraw(500)
+      expect(account.balance).to equal(500)
+    end
+
+    xit 'should show the date of the withdrawal' do
+      account.withdraw(500)
+    end
   end
 
-  it 'should show the date of the deposit' do
-    account.deposit(1000)
-    'expect(account.withdrawal_date).to equal(date)'
-  end
-
-  it 'should issue a withdrawal' do
-    account.deposit(1000)
-    account.withdraw(500)
-    expect(account.balance).to equal(500)
-  end
-
-  it 'should not go into negative balance' do
-    account.withdraw(500)
-    expect(account.balance).to equal(0)
+  context 'account actions not requiring deposit' do
+    it 'should not go into negative balance' do
+      account.withdraw(500)
+      expect(account.balance).to equal(0)
+    end
   end
 end
